9a5bd56bfa713e28dffd875603be0ab6d926e8f0
deepak <deepak52567@gmail.com>
2018-12-30 16:22:29 +0000

    chnages css version

--- header.php
+++ header.php
@@ -11,9 +11,9 @@
         function add_theme_scripts() {
           wp_enqueue_style( 'style', get_stylesheet_uri() );
 
-          wp_enqueue_style( 'slider', get_template_directory_uri() . '/stylesheets/style.css', array(), '1.1', 'all');
+          wp_enqueue_style( 'slider', get_template_directory_uri() . '/stylesheets/style.css', array(), '1.2', 'all');
 
-          wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
+          wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.2, true);
 
             if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
               wp_enqueue_script( 'comment-reply' );
