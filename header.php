cd2a624addabe5bb771f7d9f5c2c3a8c16abf98b
deepak <deepak52567@gmail.com>
2018-12-30 21:54:40 +0530

    altered stylesheet

--- header.php
+++ header.php
@@ -9,9 +9,7 @@
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php
         function add_theme_scripts() {
-          wp_enqueue_style( 'style', get_stylesheet_uri() );
-
-          wp_enqueue_style( 'slider', get_template_directory_uri() . '/stylesheets/style.css', array(), '1.1', 'all');
+          wp_enqueue_style( 'style', get_stylesheet_uri() . '/stylesheets/style.css', array(), '1.1', 'all');
 
           wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
 
