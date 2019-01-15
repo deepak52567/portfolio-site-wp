5b5f4f64aba8ee196e3a440f691b3cad4ddbfd75
deepak <deepak52567@gmail.com>
2019-01-15 17:17:46 +0000

    changes in

--- footer.php
+++ footer.php
@@ -1,18 +1,6 @@
 <footer>
     <div class="texture-1"></div>
     <div class="texture-2"></div>
-    <?php $social_links = get_field('social_links'); ?>
-    <div class="social-con">
-        <a href="<?php echo $social_links['facebook']; ?>" target="_blank">
-            <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="<?php echo $social_links['instagram']; ?>" target="_blank">
-            <i class="fab fa-instagram"></i>
-        </a>
-        <a href="<?php echo $social_links['skype']; ?>" target="_blank">
-        <i class="fab fa-linkedin"></i> 
-        </a>
-    </div>
     <?php $main_top_page = get_field('cover_image'); ?>
     <div class="details-sec">
         2018 | <?php echo $main_top_page['first_name']; ?> <?php echo $main_top_page['last_name']; ?>
